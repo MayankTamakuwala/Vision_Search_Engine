b04de702841f99495cba9b5644dd7af216f6461a
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2023-10-23 17:17:29 -0700

    AND NOT implemented (Not Tested by Prof)

--- querying/andquery.py
+++ querying/andquery.py
@@ -2,20 +2,21 @@ from .querycomponent import QueryComponent
 from indexing import Index, Posting
 from copy import deepcopy
 
+
 class AndQuery(QueryComponent):
-    def __init__(self, components : list[QueryComponent]):
+    def __init__(self, components: list[QueryComponent]):
         # please don't rename the "components" field.
         self.components = components
 
-    def get_postings(self, index : Index, processor) -> list[Posting]:
+    def get_postings(self, index: Index, processor) -> list[Posting]:
         # TODO: program the merge for an AndQuery, by gathering the postings of the composed QueryComponents and
         #  intersecting the resulting postings.
         temp = []
         for i in self.components:
             temp.append(i.get_postings(index, processor))
 
-        for _ in range(temp.count(None)):
-            temp.remove(None)
+        # for _ in range(temp.count(None)):
+        #     temp.remove(None)
 
         if len(temp) > 0:
             p1 = temp[0]
@@ -25,19 +26,41 @@ class AndQuery(QueryComponent):
                 pointer1 = 0
                 pointer2 = 0
                 mergeList = []
-                while pointer1 < len(p1) and pointer2 < len(p2):
-                    if p1[pointer1].get_doc_id() < p2[pointer2].get_doc_id():
-                        pointer1 += 1
+                if (p1 is not None) and (p2 is not None):
+                    while pointer1 < len(p1) and pointer2 < len(p2):
+                        if self.components[postingTrack].is_positive():
+                            if p1[pointer1].get_doc_id() < p2[pointer2].get_doc_id():
+                                pointer1 += 1
 
-                    elif p2[pointer2].get_doc_id() < p1[pointer1].get_doc_id():
-                        pointer2 += 1
+                            elif p2[pointer2].get_doc_id() < p1[pointer1].get_doc_id():
+                                pointer2 += 1
 
-                    else:
+                            else:
+                                mergeList.append(Posting(p1[pointer1].get_doc_id()))
+                                pointer1 += 1
+                                pointer2 += 1
+                        else:
+                            if p1[pointer1].get_doc_id() < p2[pointer2].get_doc_id():
+                                mergeList.append(Posting(p1[pointer1].get_doc_id()))
+                                pointer1 += 1
+
+                            elif p2[pointer2].get_doc_id() < p1[pointer1].get_doc_id():
+                                # mergeList.append(Posting(p2[pointer2].get_doc_id()))
+                                pointer2 += 1
+
+                            else:
+                                pointer1 += 1
+                                pointer2 += 1
+
+                    while pointer1 < len(p1):
                         mergeList.append(Posting(p1[pointer1].get_doc_id()))
                         pointer1 += 1
+
+                    while pointer2 < len(p2):
+                        mergeList.append(Posting(p2[pointer2].get_doc_id()))
                         pointer2 += 1
 
-                p1 = deepcopy(mergeList)
+                    p1 = deepcopy(mergeList)
                 postingTrack += 1
 
             result = deepcopy(p1)
